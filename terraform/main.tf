f91de673cea387b043be925ca1dbd2d083b9f0f8
ei-sei <sk.ahm2019@gmail.com>
2026-07-14 15:26:46 +0100

    fix: add route to route table and az/public IP to subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,12 +7,18 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 }
 
 resource "aws_route_table_association" "main" {
