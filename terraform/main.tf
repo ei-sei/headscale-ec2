cf800523ab23c273ae96eb67eb762510e755f554
ei-sei <sk.ahm2019@gmail.com>
2026-07-15 23:18:27 +0100

    feat: add security group for headscale control plane

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,23 @@ resource "aws_route_table_association" "main" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
 }
+
+resource "aws_security_group" "main" {
+  name        = "main_sg"
+  description = "Headscale Control plane"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
